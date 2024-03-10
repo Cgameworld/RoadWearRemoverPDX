157e61ab9066fbbf310dd287686105bcda865196
Cgameworld <support@cgameworld.com>
2024-03-09 22:03:18 -0800

    remove logs

--- Mod.cs
+++ Mod.cs
@@ -10,18 +10,9 @@ namespace RoadWearRemoverPDX
 {
     public class Mod : IMod
     {
-        //public static ILog log = LogManager.GetLogger($"{nameof(RoadWearRemoverPDX)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
-
         public void OnLoad(UpdateSystem updateSystem)
         {
-            //log.Info(nameof(OnLoad));
-
-            //if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
-               // log.Info($"11 Current mod asset at {asset.path}");
-
             updateSystem.UpdateAt<RoadWearRemoverSystem>(SystemUpdatePhase.PostSimulation);
-
-           // log.Info("system updated?");
         }
 
         public void OnDispose() { }
