662ec9ec9f93bcb9b35aa79a1816c6095b023b9c
Cgameworld <support@cgameworld.com>
2024-03-09 22:01:30 -0800

    Add project files.

---
+++ Mod.cs
@@ -0,0 +1,29 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.Rendering;
+using Game.SceneFlow;
+using Unity.Entities;
+using UnityEngine;
+
+namespace RoadWearRemoverPDX
+{
+    public class Mod : IMod
+    {
+        //public static ILog log = LogManager.GetLogger($"{nameof(RoadWearRemoverPDX)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            //log.Info(nameof(OnLoad));
+
+            //if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+               // log.Info($"11 Current mod asset at {asset.path}");
+
+            updateSystem.UpdateAt<RoadWearRemoverSystem>(SystemUpdatePhase.PostSimulation);
+
+           // log.Info("system updated?");
+        }
+
+        public void OnDispose() { }
+    }
+}
---
+++ RoadWearRemoverSystem.cs
@@ -0,0 +1,23 @@
+﻿using Colossal.Logging;
+using Colossal.Serialization.Entities;
+using Game;
+using Game.Audio;
+using Game.Prefabs;
+using Game.Rendering;
+using Game.Simulation;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+namespace RoadWearRemoverPDX
+{
+    public partial class RoadWearRemoverSystem : GameSystemBase
+    {
+        protected override void OnGameLoaded(Context serializationContext) {
+            Shader shader = Shader.Find("BH/Decals/CurvedDecalDeteriorationShader");
+            World.GetExistingSystemManaged<RenderingSystem>().SetShaderEnabled(shader, false);
+        }
+
+        protected override void OnUpdate() { }
+    }
+}
